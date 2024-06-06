88b9cc8c278c01dd7084ab95e29edee1adb43587
Jaime Markkern <jaimemarkkern@gmail.com>
2024-06-06 13:23:13 -0700

    more setting up

--- myApp/src/pages/Tab2.tsx
+++ myApp/src/pages/Tab2.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonDatetime, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
@@ -16,6 +16,7 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">Calendar</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonDatetime></IonDatetime>
       </IonContent>
     </IonPage>
   );
