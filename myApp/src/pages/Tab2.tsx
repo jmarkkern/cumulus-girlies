49246872184d3d0bef67fa709f2921cccc944099
Jaime Markkern <jaimemarkkern@gmail.com>
2024-06-06 15:04:48 -0700

    button styling for tab2

--- myApp/src/pages/Tab2.tsx
+++ myApp/src/pages/Tab2.tsx
@@ -1,6 +1,7 @@
-import { IonContent, IonDatetime, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonDatetime, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import { useState } from 'react';
 
 const Tab2: React.FC = () => {
   return (
@@ -16,7 +17,9 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">Calendar</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonDatetime></IonDatetime>
+        <IonButton>Monthly</IonButton>
+        <IonButton>Weekly</IonButton>
+        <IonButton>Daily</IonButton>
       </IonContent>
     </IonPage>
   );
